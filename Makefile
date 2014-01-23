6164f31db66fe0071a0cc15617377427fe000974
Thomas Levine <occurrence@thomaslevine.com>
2014-01-23 19:17:53 +0000

    attendees.py

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 attendees.txt: open_government_meeting.html
-	./attendees.py
+	./attendees.py > attendees.txt
 
 open_government_meeting.html:
 	wget -O open_government_meeting.html https://public.resource.org/open_government_meeting.html
---
+++ attendees.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import lxml.html
+
+def main():
+    html = lxml.html.parse('open_government_meeting.html')
+    trs = html.xpath('//table[@width="100%"]/tbody/tr[position()>1]')
+    for to in map(email_address, trs):
+        print(to)
+
+def email_address(tr):
+    name = str(tr.xpath('td[position()=2]')[0].text_content())
+    addr = str(tr.xpath('td[position()=3]')[0].text_content())\
+        .replace(' [at] ','@').replace(' [dot] ','.')
+    return '%s <%s>' % (name, addr)
+
+if __name__ == '__main__':
+    main()
